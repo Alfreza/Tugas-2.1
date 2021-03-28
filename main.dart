c1353b6e344d40e39e4c1069d0bd59a2a0f754fe
Alfreza <80263963+Alfreza@users.noreply.github.com>
2021-03-28 13:31:26 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_6/alreza.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: alfreza());
+  }
+}
